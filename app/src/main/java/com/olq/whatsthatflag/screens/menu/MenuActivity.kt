112a151ec1ec7b410a74f63a6cc54782c8756a2b
Aleksander Woźniak <aleksanderwozniak96@gmail.com>
2017-11-24 20:21:03 +0100

    Added App icon, fixed MenuScreen button listener

--- app/src/main/java/com/olq/whatsthatflag/screens/menu/MenuActivity.kt
+++ app/src/main/java/com/olq/whatsthatflag/screens/menu/MenuActivity.kt
@@ -27,19 +27,22 @@ class MenuActivity : AppCompatActivity() {
 
         mySeekBarText.text = getString(R.string.countries_amount, "20")
         setSeekBarListener()
+        setStartBtnListener()
 
         // Make one of radio buttons selected
         radioGlobal.callOnClick()
     }
 
 
-    fun onStartBtnClick(view: View) {
-        val amount = calculateAmountOfCountries(myCountriesSeekBar.progress)
-        val selectedContinent = getSelectedContinent((radioGroupContinents as RadioGroupTableLayout).getCheckedRadioButtonId())
+    private fun setStartBtnListener() {
+        myStartBtn.setOnClickListener {
+            val amount = calculateAmountOfCountries(myCountriesSeekBar.progress)
+            val selectedContinent = getSelectedContinent((radioGroupContinents as RadioGroupTableLayout).getCheckedRadioButtonId())
 
-        startActivity<GameActivity>(
-                "AMOUNT_OF_COUNTRIES" to amount,
-                "SELECTED_CONTINENT" to selectedContinent)
+            startActivity<GameActivity>(
+                    "AMOUNT_OF_COUNTRIES" to amount,
+                    "SELECTED_CONTINENT" to selectedContinent)
+        }
     }
 
 
